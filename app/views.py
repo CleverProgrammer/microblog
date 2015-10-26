f7385f20c5e397a46bd467c6fa05c0acffc2d69f
rafeh01 <rafehqazi1@gmail.com>
2015-10-26 11:59:12 -0500

    added posts. added more jinja2 features.

--- app/views.py
+++ app/views.py
@@ -1,17 +1,27 @@
+from flask import render_template
 from app import app
+from random import choice
 
 @app.route('/')
 @app.route('/index')
 def index():
-    user = {'nickname': 'Qazi'}
-    return '''
-<html>
-    <head>
-        <title>Home Page</title>
-    </head>
-    <body>
-        <h1>Hello, ''' + user['nickname'] + '''</h1>
-    </body>
-</html>
-'''
+    user = choice([{'nickname': 'Qazi'}, {'nickname': 'James'}])
+    posts = [
+        {
+            'author': {'swagname': 'Magnus'},
+            'body': 'A beautiful chess game!'
+        },
+        {
+            'author': {'swagname': 'Carlsen'},
+            'body': 'An immortal game that one must see...'
+        },
+        {
+            'author': {'swagname': 'Qazi'},
+            'body': 'Chess is pretty cool.'
+        }
+    ]
+    return render_template('index.html',
+                           title='Home',
+                           user=user,
+                           posts=posts)
 
