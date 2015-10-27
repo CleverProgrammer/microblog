37c6e6b5179ca588871eed6dca3d7522cab655f2
rafeh01 <rafehqazi1@gmail.com>
2015-10-27 08:46:17 -0500

    Added models.py. Also added a basic database using WWW SQL Designer tool.

--- app/models.py
+++ app/models.py
@@ -1,1 +1,12 @@
-__author__ = 'ChessTastic'
+from app import db
+
+# Used WWW SQL Designer tool to sketch my idea
+# Link: http://ondras.zarovi.cz/sql/demo/
+# Click load and enter: rafeh01
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    nickname = db.Column(db.String(64), index=True, unique=True)
+    email = db.Column(db.String(120), index=True, unique=True)
+
+    def __repr__(self):
+        return '<user %r>' % (self.nickname)
