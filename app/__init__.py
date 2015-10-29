d986fe4806e4cbaca527dd39dc50df4f13242164
rafeh01 <rafehqazi1@gmail.com>
2015-10-29 09:01:41 -0500

    added email support and logging support in app/__init__.py.

--- app/__init__.py
+++ app/__init__.py
@@ -16,6 +16,9 @@ oid = OpenID(app, os.path.join(basedir, 'tmp'))
 
 if not app.debug:
     import logging
+    from logging.handlers import RotatingFileHandler
+
+    # email admin if there is an error
     from logging.handlers import SMTPHandler
     credentials = None
     if MAIL_USERNAME or MAIL_PASSWORD:
@@ -24,5 +27,13 @@ if not app.debug:
                                ADMINS, 'microblog failure', credentials)
     mail_handler.setLevel(logging.ERROR)
     app.logger.addHandler(mail_handler)
+
+    # log errors
+    file_handler = RotatingFileHandler('tmp/microblog.log', 'a', 1 * 1024 * 1024, 10)
+    file_handler.setFormatter(logging.Formatter('%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'))
+    app.logger.setLevel(logging.INFO)
+    file_handler.setLevel(logging.INFO)
+    app.logger.addHandler(file_handler)
+    app.logger.info('microblog startup')
 
 from app import views, models
