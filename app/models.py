70fb16e93a1841f2f33afd2ac634d2fa136465db
rafeh01 <rafehqazi1@gmail.com>
2015-10-29 06:59:19 -0500

    added 404 and 500 error html templates. Changed nickname index to False and unique to False in models.py. Need to fix that immediately after playing around with it.

--- app/models.py
+++ app/models.py
@@ -6,7 +6,7 @@ from hashlib import md5
 # Click load and enter: rafeh01
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    nickname = db.Column(db.String(64), index=True, unique=True)
+    nickname = db.Column(db.String(64), index=False, unique=False)
     email = db.Column(db.String(120), index=True, unique=True)
     posts = db.relationship('Post', backref='author', lazy='dynamic')
     about_me = db.Column(db.String(140))
